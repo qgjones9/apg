c628402c48f218abc23eb0a70166c2266ca89b65
Quincy Jones <quincy@qgjones9.com>
2025-04-20 10:56:37 -0400

    create art details component

---
+++ src/app/components/art-details/art-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-art-details',
+  imports: [],
+  templateUrl: './art-details.component.html',
+  styleUrl: './art-details.component.css'
+})
+export class ArtDetailsComponent {
+
+}
