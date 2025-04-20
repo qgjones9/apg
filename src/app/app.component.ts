c78ce3a5baf72831e5094fe47b1d236c90eb6a99
Quincy Jones <quincy@qgjones9.com>
2025-04-20 10:55:27 -0400

    create art gallery component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GalleryComponent } from "./components/gallery/gallery.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [GalleryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
